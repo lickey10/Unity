ca520f7f16fefaf4a653d3442f683adfd87565db
lickey10 <lickey10@gmail.com>
2019-05-27 23:08:10 -0600

    First Import

---
+++ MX Adventure/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.MiniJSON;
+
+public class GameOverScript : MonoBehaviour {
+	public GUIStyle customGuiStyle;
+	public Sprite[] Backgrounds;
+	public GUISkin MenuSkin;
+
+	int logoX;
+	int logoY;
+	private string fbMessage = "";
+	private System.Exception fbException;
+	private bool sharedToFB = false;
+
+	void Awake()
+	{
+		FB.Init(SetInit,OnHideUnity);
+	}
+
+	private void SetInit()
+	{
+		enabled = true;
+		if(FB.IsLoggedIn)
+		{
+			OnLoggedIn();
+		}
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		if(!isGameShown)
+		{
+			Time.timeScale = 0;
+		}
+		else
+			Time.timeScale = 1;
+	}
+
+	void LoginCallback(FBResult result)
+	{
+		if(FB.IsLoggedIn)
+			OnLoggedIn();
+	}
+
+	void OnLoggedIn()
+	{
+
+	}
+
+
+	void Start()
+	{
+		gamestate.Instance.SetGameRunning(false);
+		
+		try {
+			//save gamestate to player prefs
+			gamestate.Instance.Save("GameState");
+		} catch (System.Exception ex) {
+			string message = ex.Message;
+		}
+
+		//set background
+		GameObject.FindGameObjectWithTag("background").GetComponent<SpriteRenderer>().sprite = Backgrounds[gamestate.Instance.getActiveLevel() - 1];
+	}
+
+	void OnStart()
+	{
+//		gamestate.Instance.SetGameRunning(false);
+//		
+//		try {
+//			//save gamestate to player prefs
+//			gamestate.Instance.Save("GameState");
+//		} catch (System.Exception ex) {
+//			string message = ex.Message;
+//		}
+
+		customGuiStyle = new GUIStyle();
+		
+		customGuiStyle.font = (Font)Resources.Load("Fonts/advlit");
+		customGuiStyle.active.textColor = Color.red; // not working
+		customGuiStyle.hover.textColor = Color.blue; // not working
+		customGuiStyle.normal.textColor = Color.green;
+		customGuiStyle.fontSize = 50;
+		
+		customGuiStyle.stretchWidth = true; // ---
+		customGuiStyle.stretchHeight = true; // not working, since backgrounds aren't showing
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnGUI()
+	{
+		logoX = (Screen.width - 300 ) / 2;
+		logoY = (Screen.height - 450) / 2;
+
+		//logo
+		//drop shadow
+		customGuiStyle.normal.textColor = Color.black;
+		GUI.Box(new Rect( logoX+3, logoY+3, 450, 30 ), "Game Over!" ,customGuiStyle);
+		
+		customGuiStyle.normal.textColor = Color.green;
+		GUI.Box(new Rect( logoX, logoY, 450, 30 ), "Game Over!" ,customGuiStyle);
+
+		GUI.skin = MenuSkin;
+		
+		if (!FB.IsLoggedIn)
+		{			
+			if (GUI.Button(new Rect(
+				(Screen.width-200)/2,
+				Screen.height - 65,
+				200,
+				60
+				), "", MenuSkin.GetStyle("button_login")))
+			{
+				try {
+					FB.Login("publish_actions", LoginCallback);
+				} catch (System.Exception ex) {
+					fbException = ex;
+					fbMessage = ex.Message;
+					
+				}
+			}
+		}
+		else if(!sharedToFB) //display share dialog
+		{
+			try {
+				//check for a new highscore and modify message
+				string newHighScoreMessage = "";
+				string newHigherLevelMessage = "";
+				
+				if(gamestate.Instance.NewHighScore())
+				{
+					newHighScoreMessage = "I just got a new High Score on MX Adventure: "+ gamestate.Instance.GetScore() +"! Can you beat it?";
+				}
+				else if(gamestate.Instance.NewHigherLevel())
+				{
+					newHigherLevelMessage = "I just reached level "+ gamestate.Instance.getLevelProgress() +"! Can you beat it?";
+				}
+				else
+					newHighScoreMessage = "I got a score of "+ gamestate.Instance.GetScore() +" on MX Adventure!";
+
+				
+				
+				
+				FB.Feed(
+					linkCaption: newHighScoreMessage + newHigherLevelMessage,
+					//picture: "http://www.promotocross.com/sites/default/files/WPS_FLYracing_logos_640x120_v3.jpg",
+					linkName: "Checkout my MX Skills!",
+					link: "https://play.google.com/store/apps/details?id=com.SnickleFritz.MXAdventure"
+					);
+				
+				sharedToFB = true;
+			} catch (System.Exception ex) {
+				print(ex.Message);
+			}
+		}
+	}
+}
---
+++ MX Adventure/Assets/Scripts/ScoreAndLives.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAndLives : MonoBehaviour {
+
+	//generate generateScript;
+
+	// Use this for initialization
+	void Start () {
+		//generateScript = (generate)FindObjectOfType(typeof(generate));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(generateScript != null)
+//		{
+//			if(!generateScript.generating)
+//			{
+//				Invoke("endLevel",7f);
+//			}
+//		}
+	}
+
+	void endLevel()
+	{
+		//the level is over
+		GameObject thePlayer = GameObject.Find("player");
+
+		if(thePlayer != null)
+		{
+			//send them flying off the right of the screen
+			thePlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(300, 0));
+		}
+	}
+
+	//display score and lives
+	void OnGUI () 
+	{
+		GUIStyle labelStyle = new GUIStyle();
+		labelStyle.fontSize = 28;
+
+		//display score
+		labelStyle.normal.textColor = Color.green;
+		GUI.Label(new Rect (5, 10 + gamestate.Instance.GetBannerHeight(), 100, 50), "Score: "+ gamestate.Instance.GetScore(), labelStyle);
+
+		//display lives
+		labelStyle.normal.textColor = Color.green;
+		labelStyle.alignment = TextAnchor.UpperRight;
+		GUI.Label(new Rect (Screen.width-110, 10 + gamestate.Instance.GetBannerHeight(), 100, 50), "Lives: "+ gamestate.Instance.getLives() +" ", labelStyle);
+
+		//display high score
+		labelStyle.normal.textColor = Color.black;
+		GUI.Label(new Rect ((Screen.width-100)/2, 10 + gamestate.Instance.GetBannerHeight(), 100, 50), "Hi Score: "+ gamestate.Instance.getHighScore(), labelStyle);
+	}
+}
