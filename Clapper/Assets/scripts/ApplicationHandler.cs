2e748b6d37b45ba2b3b3950981a8a26206f39e83
lickey10 <lickey10@gmail.com>
2019-05-30 22:33:14 -0600

    First Import

---
+++ Clapper/Assets/scripts/ApplicationHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AndroidJavaClass pluginClass = new AndroidJavaClass("android.content.pm.PackageManager");
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        int flag = pluginClass.GetStatic<int>("GET_META_DATA");
+        AndroidJavaObject pm = currentActivity.Call<AndroidJavaObject>("getPackageManager");
+        AndroidJavaObject syspackages = pm.Call<AndroidJavaObject>("getInstalledApplications", flag);
+
+        //Put the file in a folder called StreamingAssets. The file will then be included in the data files that come along with the launcher.The path of the executable at runtime can be determined like so:
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "fceux.exe");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "/fceux/fceux.exe");
+        System.Diagnostics.Process.Start(filePath);
+    }
+}
---
+++ Clapper/Assets/scripts/MicrophoneInput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour {
+	public float sensitivity = 100;
+	public float loudness = 0;
+    private string audioSource = "";
+	
+	void Start() {
+		GetComponent<AudioSource>().clip = Microphone.Start(null, true, 10, 44100);
+		GetComponent<AudioSource>().loop = true; // Set the AudioClip to loop
+		GetComponent<AudioSource>().mute = true; // Mute the sound, we don't want the player to hear it
+		//while (!(Microphone.GetPosition(GetComponent<AudioSource>().name) > 0)){} // Wait until the recording has started
+        while (!(Microphone.GetPosition(null) > 0)) { } // Wait until the recording has started
+        GetComponent<AudioSource>().Play(); // Play the audio source!
+
+        audioSource = GetComponent<AudioSource>().name;
+    }
+	
+	void Update(){
+		loudness = GetAveragedVolume() * sensitivity;
+
+        if(loudness != 0)
+        {
+            print("yeah");
+        }
+	}
+
+    void OnGUI()
+    {
+        var style = new GUIStyle("label");
+        style.fontSize = 80;
+
+        GUI.Label(new Rect(175, 0, 600, 400), "soundLevel:" + loudness.ToString(), style);
+        GUI.Label(new Rect(175, 400, 600, 400), "AudioSource:" + audioSource, style);
+    }
+
+    float GetAveragedVolume()
+	{ 
+		float[] data = new float[256];
+		float a = 0;
+		GetComponent<AudioSource>().GetOutputData(data,0);
+		foreach(float s in data)
+		{
+			a += Mathf.Abs(s);
+		}
+		return a/256;
+	}
+}
