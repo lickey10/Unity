2bb0318a153a628cba94512ffa7a762acacb10ac
lickey10 <lickey10@gmail.com>
2019-06-09 13:49:41 -0600

    First Import

---
+++ Ghost Hunter - EMF/Assets/scripts/AndroidHelperClasses/ProximitySensorHelperClass.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximitySensorHelperClass : MonoBehaviour {
+
+	private static ProximitySensorHelperClass _instance;
+	string theError = "";
+	string theError2 = "";
+	public AndroidJavaObject ProximitySensor;
+	private AndroidJavaObject playerActivityContext = null;
+
+	public static ProximitySensorHelperClass Instance
+	{
+		get
+		{
+			if(_instance == null)
+				_instance = new ProximitySensorHelperClass();
+			
+			return _instance;
+		}
+	}
+	
+	public ProximitySensorHelperClass()
+	{
+		ProximitySensor = new AndroidJavaObject ("com.snicklefritz.sensorplugin.ProximitySensor");
+
+		// First, obtain the current activity context
+		using (var actClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			playerActivityContext = actClass.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+
+		ProximitySensor.Call("SetCurrentContext",playerActivityContext);
+	}
+	
+	public float GetProximityIntensityValue()
+	{
+		return ProximitySensor.Call<float>("GetProximityValue");
+	}
+	
+	void OnApplicationQuit()
+	{
+		_instance = null;
+	}
+}
