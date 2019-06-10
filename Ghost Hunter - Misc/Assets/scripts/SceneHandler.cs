06a45d2033bcfa308367cb17633c7eb44c9e01f6
lickey10 <lickey10@gmail.com>
2019-06-09 18:54:30 -0600

    First Import

---
+++ Ghost Hunter - Misc/Assets/scripts/SceneHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisplayInfoScene()
+    {
+        Application.LoadLevel("InfoScene");
+    }
+
+    public void DisplayMiscData()
+    {
+        Application.LoadLevel("main");
+    }
+}
