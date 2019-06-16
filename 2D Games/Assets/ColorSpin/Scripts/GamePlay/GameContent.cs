e6dcee5da3a313f19c80e021681c4632e4940a89
lickey10 <lickey10@gmail.com>
2019-06-15 21:25:41 -0600

    removed stuff

--- 2D Games/Assets/ColorSpin/Scripts/GamePlay/GameContent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameContent : MonoBehaviour 
-{
-	public GameObject playingContent;
-	public GameObject animatingContent;
-}
